e73f73882d453f92f1b149ce109d02a9f72b727c
JonathanVerberne <56781613+JonathanVerberne@users.noreply.github.com>
2022-09-18 17:10:41 +1000

    Add files via upload

---
+++ EF.Core.Options.Pattern/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EF.Core.Options.Pattern.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; private set; }
+
+        public string EmployeeName { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+        public DateTime Created { get; private set; } = DateTime.Now;
+    }
+}
